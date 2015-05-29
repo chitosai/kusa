9a0165be9c2a0a8178a7bb7211b2a29960bf7fb0
TheC <i@thec.me>
2015-05-29 16:49:54 +0800

    finishi init script

--- blog/init.py
+++ blog/init.py
@@ -1,16 +1,37 @@
-import os
+import os, json
 
-dirs = []
+# dirs that will be created
+dirs = ['posts', 'templates', 'static']
+
+# default config file
+default_config = '''{
+	"url"         : "http://yourdomain/",
+	"name"        : "your blog name",
+	"description" : "whatever you like",
+	"author"      : "your name",
+	"feed"        : "something like /feed"
+}'''
 
 def init():
-	# check if current dir is alreay inited
+	global dirs, default_config
+
+	# put the default config file
+	# and check if current dir is alreay inited
 	try:
-		f = open('.CHITOSE_INIT_FLAG', 'r')
-		f.close()
+		f = open('config.json', 'r')
 		print 'You have already initialized Chitose Blog in this dir'
-	except:
-		f = open('.CHITOSE_INIT_FLAG', 'wb')
 		return False
+	except:
+		f = open('config.json', 'w')
+		f.write(default_config)
+	finally:
+		f.close()
 
 	# make dir
-
+	cur_dir = os.path.abspath('.')
+	for dir_name in dirs:
+		new_dir = os.path.join(cur_dir, dir_name)
+		try:
+			os.mkdir(new_dir)
+		except:
+			print 'Fail to create %s, path already exists?' % str(new_dir)
